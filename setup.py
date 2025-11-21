fce0de801d8a4f9367a38f1bbc130e51f81b3d4a
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-11-21 10:05:24 +0200

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Setup script for supreme-waddle."""
+
+from setuptools import setup
+
+setup(
+    name="supreme-waddle",
+    version="1.0.0",
+    description="A simple Python project",
+    py_modules=["main", "utils"],
+)
+
